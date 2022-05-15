987ecfc5f7f02afe699bcd8bf289a2f4f4a926c4
angmont <angela.montoyaldp@uanl.edu.mx>
2022-05-14 22:08:21 -0500

    Version final
    ultima version

--- vulnerables.py
+++ vulnerables.py
@@ -13,6 +13,9 @@ mensajes = []
 for i in lineas:
     mensajes.append(i[:8])
 
+#VULNERABILIDAD 1
+'''
+print('Vulnerabilidad 1')
 for i in llaves:
     cont = 0
     for j in mensajes:
@@ -22,5 +25,23 @@ for i in llaves:
             cont = cont + 1
     if cont == 256:
         print(i)
+'''
+print('Vulnerabilidad 2')
+angela = llaves[:340]
+dari = llaves[340:681]
+ian = llaves [681:1024]
+
+for i in llaves:
+    for j in llaves:
+        cont = 0
+        for x in mensajes:
+            enc1 = encriptado.encriptado(x, i)
+            enc2 = encriptado.encriptado(enc1, j)
+            if enc2 == x:
+                cont = cont + 1
+            if enc2 != x:
+                break
+        if cont == 256:
+            print(i, j)
 
 
